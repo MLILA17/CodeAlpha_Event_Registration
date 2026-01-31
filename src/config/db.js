70d51a76a63f85ec831ad0cc539a3636d8bac9d9
DML <daudymussa1705@gmail.com>
2026-01-31 05:49:59 -0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error(error.message);
+    console.error("❌ MongoDB Error:", error.message);
     process.exit(1);
   }
 };
